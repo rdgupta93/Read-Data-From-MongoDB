6a03a03e6137a06a35082e14cbdcdd930a707db3
rdgupta93 <110840567+rdgupta93@users.noreply.github.com>
2023-02-25 16:58:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const dbConnect = require('./mongodb')
+
+const main = async()=>{
+    let data = await dbConnect();
+    data =await data.find().toArray();
+    console.log(data);
+}
+main();
+
+
+
+
